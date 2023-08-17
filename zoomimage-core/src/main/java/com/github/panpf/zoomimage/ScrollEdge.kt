193685b7e1ac4fbd0fd74bfa0e92456d5cc08402
panpf <panpfpanpf@outlook.com>
2023-08-17 17:50:23 +0800

    new: ScrollEdge added Default

--- zoomimage-core/src/main/java/com/github/panpf/zoomimage/ScrollEdge.kt
+++ zoomimage-core/src/main/java/com/github/panpf/zoomimage/ScrollEdge.kt
@@ -4,7 +4,11 @@ enum class Edge {
     NONE, START, END, BOTH
 }
 
-data class ScrollEdge(val horizontal: Edge, val vertical: Edge)
+data class ScrollEdge(val horizontal: Edge, val vertical: Edge) {
+    companion object {
+        val Default: ScrollEdge = ScrollEdge(Edge.BOTH, Edge.BOTH)
+    }
+}
 
 // todo Unit tests
 fun ScrollEdge.toShortString(): String = "(${horizontal.name},${vertical.name})"
