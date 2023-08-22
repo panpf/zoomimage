5b3e7d6d9e68b2a6b4350b57afb713868c561cc1
panpf <panpfpanpf@outlook.com>
2023-08-23 00:55:03 +0800

    build: Added publish config

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,31 @@ plugins {
     alias(libs.plugins.com.android.library) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
     alias(libs.plugins.androidx.navigation.safeargs.kotlin) apply false
+    alias(libs.plugins.mavenpublish) apply false
 }
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+allprojects {
+    /**
+     * publish config
+     */
+    if (hasProperty("signing.keyId")    // configured in the ~/.gradle/gradle.properties file
+        && hasProperty("signing.password")    // configured in the ~/.gradle/gradle.properties file
+        && hasProperty("signing.secretKeyRingFile")    // configured in the ~/.gradle/gradle.properties file
+        && hasProperty("mavenCentralUsername")    // configured in the ~/.gradle/gradle.properties file
+        && hasProperty("mavenCentralPassword")    // configured in the ~/.gradle/gradle.properties file
+        && hasProperty("versionName")    // configured in the rootProject/gradle.properties file
+        && hasProperty("GROUP")    // configured in the rootProject/gradle.properties file
+        && hasProperty("POM_ARTIFACT_ID")    // configured in the project/gradle.properties file
+    ) {
+        apply { plugin("com.vanniktech.maven.publish") }
+
+        configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
+            version = property("versionName").toString()
+            publishToMavenCentral("S01")
+        }
+    }
 }
