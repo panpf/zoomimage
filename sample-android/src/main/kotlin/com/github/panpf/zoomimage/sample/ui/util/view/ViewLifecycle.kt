75c514acdd203cf62421e0c07e596dcd3831693e
panpf <panpfpanpf@outlook.com>
2023-12-08 15:39:14 +0800

    sample: Improved sample app

--- sample-android/src/main/kotlin/com/github/panpf/zoomimage/sample/ui/util/view/ViewLifecycle.kt
+++ sample-android/src/main/kotlin/com/github/panpf/zoomimage/sample/ui/util/view/ViewLifecycle.kt
@@ -7,9 +7,16 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
 import com.github.panpf.zoomimage.sample.R
 
-internal val View.lifecycleOwner: LifecycleOwner
+val View.lifecycleOwner: LifecycleOwner
     get() {
         synchronized(this) {
+            /*
+             * After Lifecycle enters the DESTROYED state, its coroutineScope will be canceled and can no longer be used.
+             * So you must create a new Lifecycle every time onAttachedToWindow
+             */
+            check(ViewCompat.isAttachedToWindow(this)) {
+                "View.lifecycleOwner can only be called after onAttachedToWindow and before onDetachedFromWindow"
+            }
             val tag = getTag(R.id.tagId_viewLifecycle)
             if (tag != null && tag is ViewLifecycleOwner) {
                 return tag
@@ -21,7 +28,7 @@ internal val View.lifecycleOwner: LifecycleOwner
         }
     }
 
-internal class ViewLifecycleOwner(view: View) : LifecycleOwner {
+class ViewLifecycleOwner(view: View) : LifecycleOwner {
 
     private val lifecycleRegistry = LifecycleRegistry(this)
 
@@ -29,24 +36,26 @@ internal class ViewLifecycleOwner(view: View) : LifecycleOwner {
         get() = lifecycleRegistry
 
     init {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+
         view.addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
             override fun onViewAttachedToWindow(v: View) {
-                lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
-                lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
-                lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
             }
 
             override fun onViewDetachedFromWindow(v: View) {
                 lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
                 lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
                 lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+
+                /*
+                 * After Lifecycle enters the DESTROYED state, its coroutineScope will be canceled and can no longer be used.
+                 * So you must create a new Lifecycle every time onAttachedToWindow
+                 */
+                view.setTag(R.id.tagId_viewLifecycle, null)
+                view.removeOnAttachStateChangeListener(this)
             }
         })
-
-        if (ViewCompat.isAttachedToWindow(view)) {
-            lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
-            lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
-            lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
-        }
     }
 }
