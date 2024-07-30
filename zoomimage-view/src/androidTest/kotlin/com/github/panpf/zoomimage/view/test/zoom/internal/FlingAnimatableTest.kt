7fa6f2fd136949a862ab8fa9f606c82f29aae2c0
panpf <panpfpanpf@outlook.com>
2024-07-30 11:46:14 +0800

    test: Added unit tests for the zoomimage-view module

--- zoomimage-view/src/androidTest/kotlin/com/github/panpf/zoomimage/view/test/zoom/internal/FlingAnimatableTest.kt
+++ zoomimage-view/src/androidTest/kotlin/com/github/panpf/zoomimage/view/test/zoom/internal/FlingAnimatableTest.kt
@@ -1,15 +1,112 @@
 package com.github.panpf.zoomimage.view.test.zoom.internal
 
+import android.graphics.Rect
+import android.view.View
 import com.github.panpf.tools4a.test.ktx.getActivitySync
 import com.github.panpf.tools4a.test.ktx.launchActivity
 import com.github.panpf.zoomimage.test.TestActivity
+import com.github.panpf.zoomimage.util.IntOffsetCompat
+import com.github.panpf.zoomimage.view.zoom.internal.FlingAnimatable
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class FlingAnimatableTest {
 
     @Test
     fun test() {
         val activity = TestActivity::class.launchActivity().getActivitySync()
-        // TODO test
+        val view = activity.findViewById<View>(android.R.id.content)
+        val start = IntOffsetCompat(-3362, -5260)
+        val bounds = Rect(-5719, -6570, 0, -3601)
+        val velocity = IntOffsetCompat(-7773, -591)
+
+        val values1 = mutableListOf<IntOffsetCompat>()
+        runBlocking {
+            withContext(Dispatchers.Main) {
+                suspendCoroutine { continuation ->
+                    val flingAnimatable = FlingAnimatable(
+                        view = view,
+                        start = start,
+                        bounds = bounds,
+                        velocity = velocity,
+                        onUpdateValue = { value ->
+                            values1.add(value)
+                        },
+                        onEnd = {
+                            continuation.resume(Unit)
+                        }
+                    )
+                    flingAnimatable.start()
+                }
+            }
+        }
+        assertEquals(
+            expected = true,
+            actual = values1.size in 20..40
+        )
+        values1.forEachIndexed { index, it ->
+            assertEquals(
+                expected = true,
+                actual = it.x in bounds.left..bounds.right
+            )
+            assertEquals(
+                expected = true,
+                actual = it.y in bounds.top..bounds.bottom
+            )
+            if (index > 0) {
+                val lastIt = values1[index - 1]
+                assertEquals(expected = true, actual = it.x <= lastIt.x)
+                assertEquals(expected = true, actual = it.y <= lastIt.y)
+            }
+        }
+
+        val values2 = mutableListOf<IntOffsetCompat>()
+        runBlocking {
+            withContext(Dispatchers.Main) {
+                suspendCoroutine { continuation ->
+                    var flingAnimatable: FlingAnimatable? = null
+                    flingAnimatable = FlingAnimatable(
+                        view = view,
+                        start = start,
+                        bounds = bounds,
+                        velocity = velocity,
+                        onUpdateValue = { value ->
+                            values2.add(value)
+                            if(values2.size == 10) {
+                                flingAnimatable?.stop()
+                            }
+                        },
+                        onEnd = {
+                            continuation.resume(Unit)
+                        }
+                    )
+                    flingAnimatable.start()
+                }
+            }
+        }
+        assertEquals(
+            expected = 10,
+            actual = values2.size
+        )
+        values2.forEachIndexed { index, it ->
+            assertEquals(
+                expected = true,
+                actual = it.x in bounds.left..bounds.right
+            )
+            assertEquals(
+                expected = true,
+                actual = it.y in bounds.top..bounds.bottom
+            )
+            if (index > 0) {
+                val lastIt = values2[index - 1]
+                assertEquals(expected = true, actual = it.x <= lastIt.x)
+                assertEquals(expected = true, actual = it.y <= lastIt.y)
+            }
+        }
     }
 }
--- zoomimage-view/src/androidTest/kotlin/com/github/panpf/zoomimage/view/test/zoom/internal/FloatAnimatableTest.kt
+++ zoomimage-view/src/androidTest/kotlin/com/github/panpf/zoomimage/view/test/zoom/internal/FloatAnimatableTest.kt
@@ -1,5 +1,96 @@
 package com.github.panpf.zoomimage.view.test.zoom.internal
 
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import com.github.panpf.tools4a.test.ktx.getActivitySync
+import com.github.panpf.tools4a.test.ktx.launchActivity
+import com.github.panpf.zoomimage.test.TestActivity
+import com.github.panpf.zoomimage.view.zoom.internal.FloatAnimatable
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+import kotlin.math.roundToInt
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
 class FloatAnimatableTest {
-    // TODO test
+
+    @Test
+    fun test() {
+        val activity = TestActivity::class.launchActivity().getActivitySync()
+        val view = activity.findViewById<View>(android.R.id.content)
+
+        val start = 50f
+        val end = 1000f
+        val values1 = mutableListOf<Float>()
+        runBlocking {
+            withContext(Dispatchers.Main) {
+                suspendCoroutine { continuation ->
+                    val floatAnimatable = FloatAnimatable(
+                        view = view,
+                        startValue = start,
+                        endValue = end,
+                        durationMillis = 1000,
+                        interpolator = AccelerateDecelerateInterpolator(),
+                        onUpdateValue = { value ->
+                            values1.add(value)
+                        },
+                        onEnd = {
+                            continuation.resume(Unit)
+                        }
+                    )
+                    floatAnimatable.start()
+                }
+            }
+        }
+        assertEquals(expected = 62, actual = values1.size)
+        assertEquals(expected = start.roundToInt(), actual = values1.first().roundToInt())
+        assertEquals(expected = end.roundToInt(), actual = values1.last().roundToInt())
+        values1.forEachIndexed { index, it ->
+            if (index > 0) {
+                val lastIt = values1[index - 1]
+                assertEquals(expected = true, actual = it > lastIt)
+            }
+        }
+
+        val start2 = 1000f
+        val end2 = 50f
+        val values2 = mutableListOf<Float>()
+        runBlocking {
+            withContext(Dispatchers.Main) {
+                suspendCoroutine { continuation ->
+                    var floatAnimatable: FloatAnimatable? = null
+                    floatAnimatable = FloatAnimatable(
+                        view = view,
+                        startValue = start2,
+                        endValue = end2,
+                        durationMillis = 1000,
+                        interpolator = AccelerateDecelerateInterpolator(),
+                        onUpdateValue = { value ->
+                            values2.add(value)
+                            if (values2.size == 30) {
+                                floatAnimatable?.stop()
+                            }
+                        },
+                        onEnd = {
+                            continuation.resume(Unit)
+                        }
+                    )
+                    floatAnimatable.start()
+                }
+            }
+        }
+        assertEquals(expected = 30, actual = values2.size)
+        assertEquals(expected = start2.roundToInt(), actual = values2.first().roundToInt())
+        assertNotEquals(illegal = end2.roundToInt(), actual = values2.last().roundToInt())
+        values2.forEachIndexed { index, it ->
+            if (index > 0) {
+                val lastIt = values2[index - 1]
+                assertEquals(expected = true, actual = it < lastIt)
+            }
+        }
+    }
 }
