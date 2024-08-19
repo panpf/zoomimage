1ba633f4e0db9ac990959c18907aed2373f7c66c
panpf <panpfpanpf@outlook.com>
2024-08-19 16:24:28 +0800

    sample: Restore MainActivity

--- sample/src/androidMain/kotlin/com/github/panpf/zoomimage/sample/MainActivity.kt
+++ sample/src/androidMain/kotlin/com/github/panpf/zoomimage/sample/MainActivity.kt
@@ -4,20 +4,12 @@ import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.ui.Modifier
-import com.githb.panpf.zoomimage.images.ResourceImages
-import com.github.panpf.zoomimage.SketchZoomAsyncImage
+import com.github.panpf.zoomimage.sample.ui.MainFragment
 import com.github.panpf.zoomimage.sample.ui.base.BaseActivity
 import com.google.android.material.internal.EdgeToEdgeUtils
 
 class MainActivity : BaseActivity() {
 
-    @OptIn(ExperimentalFoundationApi::class)
     @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,24 +22,9 @@ class MainActivity : BaseActivity() {
             window.navigationBarColor = Color.TRANSPARENT
         }
 
-//        supportFragmentManager
-//            .beginTransaction()
-//            .replace(android.R.id.content, MainFragment())
-//            .commit()
-
-        setContent {
-            HorizontalPager(
-                state = rememberPagerState {
-                    ResourceImages.values.size
-                },
-                modifier = Modifier.fillMaxSize()
-            ) {
-                SketchZoomAsyncImage(
-                    uri = ResourceImages.values[it].uri,
-                    contentDescription = "",
-                    modifier = Modifier.fillMaxSize()
-                )
-            }
-        }
+        supportFragmentManager
+            .beginTransaction()
+            .replace(android.R.id.content, MainFragment())
+            .commit()
     }
 }
