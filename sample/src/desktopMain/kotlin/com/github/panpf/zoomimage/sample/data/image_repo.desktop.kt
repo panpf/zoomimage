605a0300db47512c6741c550b99a1d97a8d14e79
panpf <panpfpanpf@outlook.com>
2024-08-22 18:43:51 +0800

    sample: Local photo albums on desktop platforms support webp format

--- sample/src/desktopMain/kotlin/com/github/panpf/zoomimage/sample/data/image_repo.desktop.kt
+++ sample/src/desktopMain/kotlin/com/github/panpf/zoomimage/sample/data/image_repo.desktop.kt
@@ -5,6 +5,7 @@ import com.githb.panpf.zoomimage.images.ImageFile
 import com.githb.panpf.zoomimage.images.ResourceImages
 import com.github.panpf.sketch.PlatformContext
 import java.io.File
+import java.util.Locale
 
 actual suspend fun builtinImages(context: PlatformContext): List<ImageFile> {
     return listOf(
@@ -20,6 +21,8 @@ actual suspend fun builtinImages(context: PlatformContext): List<ImageFile> {
     ).plus(ResourceImages.exifs)
 }
 
+val acceptImageExtensions = setOf("jpeg", "jpg", "png", "gif", "webp", "bmp", "heic", "heif", "svg")
+
 actual suspend fun localImages(
     context: PlatformContext,
     startPosition: Int,
@@ -32,7 +35,7 @@ actual suspend fun localImages(
     userPicturesDir.walkTopDown().forEach { file ->
         if (file.isFile) {
             val extension = file.extension
-            if (extension == "jpeg" || extension == "jpg" || extension == "png" || extension == "gif") {
+            if (acceptImageExtensions.contains(extension.lowercase(Locale.getDefault()))) {
                 index++
                 if (index >= startPosition && index < startPosition + pageSize) {
                     photoList.add(file.path)
