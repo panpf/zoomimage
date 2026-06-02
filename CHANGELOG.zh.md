86c09b89036e8d14c1ca2f9430848e088eed7318
panpf <panpfpanpf@outlook.com>
2026-06-02 23:41:01 +0800

    build: Upgrade to sketch 4.5.0-beta01

--- CHANGELOG.zh.md
+++ CHANGELOG.zh.md
@@ -32,7 +32,7 @@
 * 升级 kotlinx collections immutable 0.4.0
 * 升级 kotlinx coroutines 1.11.0
 * 升级 okio 3.17.0
-* 升级 sketch 4.5.0-alpha03
+* 升级 sketch 4.5.0-beta01
 * 升级 glide 5.0.7
 
 # 1.4.0
--- samples/shared/src/iosMain/kotlin/com/github/panpf/zoomimage/sample/ui/examples/SketchZoomAsyncImageSample.ios.kt
+++ samples/shared/src/iosMain/kotlin/com/github/panpf/zoomimage/sample/ui/examples/SketchZoomAsyncImageSample.ios.kt
@@ -1,11 +1,6 @@
 package com.github.panpf.zoomimage.sample.ui.examples
 
 import com.github.panpf.sketch.request.ImageOptions
-import com.github.panpf.zoomimage.sample.ui.components.MyPhotosAssetFetcher
 
 actual fun getPlatformSketchZoomAsyncImageSampleImageOptions(): ImageOptions = ImageOptions {
-    components {
-        // TODO After the new version of Sketch is fixed, MyPhotosAssetFetcher is no longer needed.
-        add(MyPhotosAssetFetcher.Factory())
-    }
 }
